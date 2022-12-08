cbedbef53b98697d55450c9220774cefb5e9f943
Alta02 <lutfipert@gmail.com>
2022-12-08 18:32:52 +0700

    not a light mode anymore

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const sideMenu = document.querySelector("aside");
 const menuBtn = document.querySelector("#menu-btn");
 const closebtn = document.querySelector("#close-btn");
+const themeToggler = document.querySelector(".theme-toggler");
 
+// Open Sidebar
 menuBtn.addEventListener("click", () => {
   sideMenu.style.display = "block";
 });
 
+// close Sidebar
 closebtn.addEventListener("click", () => {
   sideMenu.style.display = "none";
+});
+
+// change theme
+themeToggler.addEventListener("click", () => {
+  document.body.classList.toggle(".dark-theme-variable");
+  themeToggler.querySelector("span").classList.toggle("active");
 });
